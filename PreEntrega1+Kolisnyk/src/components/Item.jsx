65043c75bed141b36847fe805eafcaa477ca3669
patriciokol <kol.patricio@gmail.com>
2024-01-28 15:57:34 -0300

    item list actualizado

--- PreEntrega1+Kolisnyk/src/components/Item.jsx
+++ PreEntrega1+Kolisnyk/src/components/Item.jsx
@@ -1,13 +1,13 @@
 const Item = ({item}) => {
     return (
-        <div className="card" style={{ width: '18rem' }}>
-          <img src={item.imagen} className="card-img-top" alt={item.alt} />
+        <div className="card mx-2 my-2" style={{ width: '18rem' }}>
+          <img src={item.imagen} className="card-img-top"/>
           <div className="card-body">
-            <h5 className="card-title">{item.titulo}</h5>
-            <p className="card-text">{item.descripcion}</p>
-            <a href={item.enlace} className="btn btn-primary">
+            <h5 className="card-title">{item.nombre}</h5>
+            <p className="card-text">${item.precio}</p>
+            {/* <a href={item.enlace} className="btn btn-primary">
               {item.enlaceTexto}
-            </a>
+            </a> */}
           </div>
         </div>
       );
